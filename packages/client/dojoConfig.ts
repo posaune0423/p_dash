1caac1d08671a97ef37decc1ca614c6752ab1716
Asuma Yamada <asuma.a.yamada@dena.com>
2024-06-21 21:39:33 +0900

    feat: :tada: create-next-app

--- packages/client/dojoConfig.ts
+++
@@ -1,6 +0,0 @@
-import manifest from "../contracts/manifests/dev/manifest.json";
-import { createDojoConfig } from "@dojoengine/core";
-
-export const dojoConfig = createDojoConfig({
-    manifest,
-});
