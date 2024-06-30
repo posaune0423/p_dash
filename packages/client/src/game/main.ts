02ec283225fe8f984ce749ba85e2b45e9d2776af
Asuma Yamada <asuma.a.yamada@dena.com>
2024-07-01 01:36:15 +0900

    fix: screen

--- packages/client/src/game/main.ts
+++ packages/client/src/game/main.ts
@@ -6,8 +6,8 @@ import { Preloader } from '@/game/scenes/Preloader'
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Phaser.Types.Core.GameConfig = {
   type: AUTO,
-  width: window.innerWidth > window.innerHeight ? window.innerWidth : window.innerHeight,
-  height: window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth,
+  width: screen.width > screen.height ? screen.width : screen.height,
+  height: screen.width > screen.height ? screen.height : screen.width,
   parent: 'game-container',
   backgroundColor: '#0f172a',
   physics: {
@@ -19,10 +19,6 @@ const config: Phaser.Types.Core.GameConfig = {
 }
 
 const StartGame = (parent: string, { stageData }: { stageData: Obstacle[] }) => {
-  console.log('=== StartGame ===')
-  console.log('innerWidth: ', window.innerWidth, 'innerHeight: ', window.innerHeight)
-  console.log('screenWidth: ', screen.width, 'screenHeight: ', screen.height)
-  console.log('clientWidth: ', document.documentElement.clientWidth, 'clientHeight: ', document.documentElement.clientHeight)
   const game = new Game({ ...config, parent })
   game.scene.add('Preloader', Preloader, true, { stageData })
   game.scene.add('MainGame', MainGame, false)
