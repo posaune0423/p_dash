fc9be8adf456e0cccba9bc85b0505f0272a0f225
Asuma Yamada <asuma.a.yamada@dena.com>
2024-07-01 02:58:45 +0900

    fix: client error

--- packages/client/src/constants/index.ts
+++ packages/client/src/constants/index.ts
@@ -3,7 +3,8 @@ import { env } from '@/env'
 
 export const APP_NAME = 'P/Dash'
 export const APP_DESCRIPTION = 'P/Dash is a game that you can play with your friends'
-export const APP_URL = env.VERCEL_ENV ? 'https://p-dash.vercel.app' : 'http://localhost:3000'
+export const APP_URL =
+  env.NEXT_PUBLIC_PROFILE === 'dev' ? 'http://localhost:3000' : 'https://p-dash.vercel.app'
 
 export const APP_DEFAULT_TITLE = 'P/Dash'
 export const APP_TITLE_TEMPLATE = 'P/Dash | %s'
