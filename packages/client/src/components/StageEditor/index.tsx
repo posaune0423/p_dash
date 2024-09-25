4e309a5da3d56398536034b77cf534d1979b7655
Asuma Yamada <yamadaasuma@gmail.com>
2024-09-25 02:03:02 -0400

    fix: remove dimension w n h

--- packages/client/src/components/StageEditor/index.tsx
+++ packages/client/src/components/StageEditor/index.tsx
@@ -3,11 +3,8 @@
 import React from 'react'
 import { StageElements } from '../StageElements'
 import { useStageEditor } from './useStageEditor'
-import { useDimension } from '@/hooks/useDimension'
 
 export const StageEditor = ({ stageId }: { stageId?: string }) => {
-  const { width, height } = useDimension()
-
   const {
     canvasRef,
     selectedElement,
@@ -19,7 +16,7 @@ export const StageEditor = ({ stageId }: { stageId?: string }) => {
   } = useStageEditor(stageId)
 
   return (
-    <main className='fixed' style={{ width, height, overflow: 'hidden' }}>
+    <main className='fixed' style={{ overflow: 'hidden' }}>
       <canvas
         ref={canvasRef}
         className='fixed inset-0 h-[calc(100%-50px)] w-full touch-none bg-black/90'
