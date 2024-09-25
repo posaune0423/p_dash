4bb554a26759d736c16ede433a5cf8ef5abdf07d
Asuma Yamada <yamadaasuma@gmail.com>
2024-09-25 03:31:01 -0400

    fix: style

--- packages/client/src/app/history/page.tsx
+++ packages/client/src/app/history/page.tsx
@@ -1,12 +1,24 @@
+'use client'
+
 import { ArrowLeft } from 'lucide-react'
 import History from '@/components/History'
 import { SoundLink } from '@/components/SoundLink'
+import { useOrientation } from '@/hooks/useOrientation'
+import { cn } from '@/utils'
 
 const HistoryPage = () => {
+  const { isLandscape } = useOrientation()
+
   return (
     <main className='fixed w-full bg-gray-800 p-4'>
       <SoundLink href='/game'>
-        <ArrowLeft className='absolute left-4 top-4 left-safe' color='white' size={36} />
+        <ArrowLeft
+          className={cn('absolute left-4 top-4', {
+            'left-safe': isLandscape,
+          })}
+          color='white'
+          size={36}
+        />
       </SoundLink>
       <h1 className='mb-8 text-center text-2xl font-bold text-white'>History</h1>
       <History />
