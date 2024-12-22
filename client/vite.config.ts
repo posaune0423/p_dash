ade2618b8c5b0efcb0a26aef0ae6ee5eee399a18
Asuma Yamada <yamadaasuma@gmail.com>
2024-12-22 15:50:03 +0900

    feat: update pwa config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,10 +4,63 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import topLevelAwait from 'vite-plugin-top-level-await'
 import wasm from 'vite-plugin-wasm'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), topLevelAwait(), wasm()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    topLevelAwait(),
+    wasm(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: false,
+      },
+      manifest: {
+        name: 'P/Dash',
+        short_name: 'P/Dash',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#1e293b',
+        theme_color: '#1e293b',
+        lang: 'en',
+        scope: '/',
+        orientation: 'landscape-primary',
+        description: 'P/Dash is a game that you can play with your friends',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 4000000, // 4MB
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
