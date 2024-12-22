00effebe9334081a98fb918e1597b392912d383b
Asuma Yamada <yamadaasuma@gmail.com>
2024-12-22 19:14:56 +0900

    fix: config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
-        enabled: false,
+        enabled: true,
       },
       manifest: {
         name: 'P/Dash',
